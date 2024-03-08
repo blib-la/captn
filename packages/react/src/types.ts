c2dbc085818964be2bb639f53c121c74cae98396
pixelass <greg@pixelass.com>
2024-03-08 13:47:20 +0100

    refactor: cleanup types

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -2,12 +2,3 @@ export interface SDKMessage<T> {
 	action: string;
 	payload: T;
 }
-
-declare global {
-	interface Window {
-		ipc: {
-			send(channel: string, value?: unknown): void;
-			on(channel: string, callback: (...args: any[]) => void): () => void;
-		};
-	}
-}
