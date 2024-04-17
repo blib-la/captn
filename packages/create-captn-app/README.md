4459157d4a0199086286a08f77ff66f089e8767a
pixelass <greg@pixelass.com>
2024-04-17 18:20:49 +0200

    feat(create-captain-app): auto generate captain id and file

---
+++ packages/create-captn-app/README.md
@@ -0,0 +1,51 @@
+# Create Captain App
+
+`create-captain-app` is a command-line utility that simplifies the creation of Captain-based Electron applications by providing a straightforward way to generate projects from a range of customizable templates. Captain is an Electron app framework designed for creating applications that can be exported as static websites, supporting a variety of web technologies.
+
+## Features
+
+- **Versatility**: Supports multiple front-end frameworks including Next.js, Svelte, Vue.js, and basic HTML + CSS.
+- **Simplicity**: Easily generate new projects with a single command, similar to `create-react-app` or `create-next-app`.
+- **Flexibility**: Create static exportable Electron apps using any web technology that supports static site generation.
+- **Template-based**: Start with a template that best fits your project needs, modifying it as required.
+
+## Prerequisites
+
+- **Node.js**: Ensure you have Node.js installed (20 or later).
+- **Git**: Git is required.
+
+## Installation
+
+To use `create-captain-app`, run the following command:
+
+```bash
+npx create-captain-app [app-name] [--template <template-url> | -t <template-url>]
+```
+
+## Usage
+
+Create a new Captain application by specifying the name of your new app and optionally, the template URL:
+
+```bash
+npx create captain-app my-captain-app
+npx create captain-app my-next-app --template https://github.com/blib-la/captain-next-app-example
+```
+### Default App Name
+
+If you do not specify an application name, create-captain-app will automatically use "my-captain-app" as the default project name.
+
+### Default Template
+
+If no template is specified, `create-captain-app` will use the default Next.js template configured for Captain.
+
+### Selecting a Template
+
+You can specify any GitHub repository URL that contains a Captain-compatible template:
+
+```bash
+npx create captain-app my-vue-app --template https://github.com/some-user/some-captain-vue-template
+```
+
+## Custom Templates
+
+To create a custom template, ensure your project is compatible with Captain and can be exported as a static site. For guidance, refer to the [default starter app](https://github.com/blib-la/captain-starter-app).
--- packages/create-captn-app/src/bin.ts
+++ packages/create-captn-app/src/bin.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
 import * as fs from "fs";
-import { mkdir, rm } from "node:fs/promises";
+import { mkdir, readFile, rm, writeFile } from "node:fs/promises";
 import path from "node:path";
 import process from "node:process";
 
 import chalk from "chalk";
 import { execa } from "execa";
 import meow from "meow";
+import { v4 } from "uuid";
 
 import { downloadAndExtractRepo, getRepoInfo, hasRepo } from "./template.js";
 
@@ -54,9 +55,53 @@ export function exitWithRepositoryError() {
 	process.exit(1);
 }
 
+const id = v4();
+
+export async function currentGitUserName() {
+	try {
+		const { stdout } = await execa("git", ["config", "user.name"]);
+		return stdout;
+	} catch (error) {
+		console.error("Error fetching Git user name:", error.message);
+		return null;
+	}
+}
+
+export async function currentGitUserEmail() {
+	try {
+		const { stdout } = await execa("git", ["config", "user.email"]);
+		return stdout;
+	} catch (error) {
+		console.error("Error fetching Git user name:", error.message);
+		return null;
+	}
+}
+
+export function createCaptainMd(username: string) {
+	return `---
+id: ${id}
+label: My Captain App
+language: en
+type: app
+creatorID: ${username}
+icon: Image
+iconColor: "#DB2777"
+parameters:
+    width: 800
+    height: 1024
+    minWidth: 800
+    minHeight: 1024
+---
+
+open the app named My Captain App
+`;
+}
+
 const newFolder = path.join(CWD, name);
 
 try {
+	const username = await currentGitUserName();
+	const userEmail = await currentGitUserEmail();
 	if (fs.existsSync(newFolder)) {
 		console.error(
 			`Folder already exists ${chalk.red(
@@ -83,6 +128,19 @@ try {
 	if (fs.existsSync(gitFolder)) {
 		await rm(gitFolder, { recursive: true });
 	}
+
+	const packagePath = path.join(newFolder, "package.json");
+	await writeFile(path.join(newFolder, "captain.md"), createCaptainMd(username));
+	const package_ = await readFile(packagePath, "utf-8");
+	const packageJson = JSON.parse(package_);
+	packageJson.name = id;
+	packageJson.version = "1.0.0";
+	packageJson.author = { name: username };
+	if (userEmail) {
+		packageJson.author.email = userEmail;
+	}
+
+	await writeFile(packagePath, JSON.stringify(packageJson, null, 2));
 
 	await execa("npm", ["install"], { cwd: newFolder, stdout: "inherit" });
 	await execa("git", ["init"], { cwd: newFolder, stdout: "inherit" });
